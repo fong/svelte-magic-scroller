1c66dc0af5ad8429ce9c200903b57bb0c23954a5
fong <enginefong@gmail.com>
2025-02-19 23:02:49 +1300

    fix exports

---
+++ src/lib/MagicScrollbar/index.js
@@ -0,0 +1,9 @@
+import MagicScrollbar from "./MagicScrollbar.svelte";
+import MagicScrollbarThumb from "./MagicScrollbarThumb.svelte";
+import MagicScrollbarTrack from "./MagicScrollbarTrack.svelte";
+
+export {
+    MagicScrollbar,
+    MagicScrollbarThumb,
+    MagicScrollbarTrack
+}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,16 @@
 // Reexport your entry components here
+import MagicScroller from "./MagicScroller.svelte";
+import MagicItem from "./MagicItem.svelte";
+import {
+    MagicScrollbar,
+    MagicScrollbarThumb,
+    MagicScrollbarTrack
+} from './MagicScrollbar/index.js';
+
+export {
+    MagicScroller,
+    MagicItem,
+    MagicScrollbar,
+    MagicScrollbarThumb,
+    MagicScrollbarTrack
+};
